0ae07532aee8e898b09188d3fb4bad1967dfcb12
ThatOneIdiot333 <ken.ma.yyc@gmail.com>
2024-12-09 21:51:34 -0700

    push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/RobotActionConfig.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.TeleOps;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class RobotActionConfig {
+    //drive chassis
+    public static double powerFactor =0.7;
+
+    public static double intake_slide_Extension = 0.55;// range(0.3 - 0.65)
+    public static double intake_slide_Retract = 0.3;
+
+    public static double intake_Rotation = 0.49;
+
+    public static double intake_Arm_initial = 0.1;//0-0.56
+    public static double intake_Arm_down = 0.05;
+    public static double intake_Arm_retract = 0.53;
+
+    public static double intake_Claw_Open = 0.55;
+    public static double intake_Claw_Close = 0.3;
+
+    //Deposit Config
+    public static int deposit_Slide_down_Pos = 50;   //slides Position Configure
+    public static int deposit_Slide_Highbar_Pos = 795;  //slides Position Configure
+    public static int deposit_Slide_Highbasket_Pos = 2800; //slides Position Configure
+
+    public static double deposit_Wrist_dump_Pos = 0.3;
+    public static double deposit_Wrist_retract_Pos = 0.15;
+
+    public static double deposit_Arm_dump_Pos = 0.8;
+    public static double deposit_Arm_retract_Pos = 0.0;
+
+    public static double deposit_Arm_hook_Pos = 0.8;
+    public static double deposit_Claw_Open = 0.11;
+    public static double deposit_Claw_Close = 0.0;
+
+    public static double dumpTime = 1.8;
+    public static double retractTime = 3.2;
+
+    public static double deposit_Slide_UpLiftPower = 0.9;  //slides power
+    public static double downLiftPower = 0.3;  //slides power
+
+    public static double deposit_Arm_Hang = 0.8;
+    public static double deposit_Wrist_Hang = 0.3;
+}
