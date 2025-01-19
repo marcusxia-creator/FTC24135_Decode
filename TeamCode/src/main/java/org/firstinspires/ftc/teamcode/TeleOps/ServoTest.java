326e7b2890e406d217d5c9960bdf43a6f464a638
gxia <xiaguowei@gmail.com>
2025-01-18 23:12:29 -0700

    Repackage FSM for deposit Ver2.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/ServoTest.java
@@ -21,7 +21,6 @@ public class ServoTest{
     private final RobotHardware robot;
 
     //declear initial servo position
-    private double servoposition;
     int iniPosition = 50;
     int delta_Position = 50;
     int current_Position;
@@ -30,6 +29,9 @@ public class ServoTest{
    //
     private final ElapsedTime debounceTimer = new ElapsedTime(); // Timer for debouncing
     private static final double DEBOUNCE_THRESHOLD = 0.25;
+    private double servoposition;
+    private double servoposition2;
+    private double servoposition3;
 
     public ServoTest(RobotHardware robot, GamepadEx gamepad_1, GamepadEx gamepad_2) {
         this.robot = robot;
@@ -176,18 +178,30 @@ public class ServoTest{
         if (gamepad_2.getButton(DPAD_LEFT) && debounceTimer.seconds() > DEBOUNCE_THRESHOLD) {
             debounceTimer.reset();
             servoposition = robot.intakeWristServo.getPosition();
+            servoposition2 = robot.intakeLeftSlideServo.getPosition();
+            servoposition3 = robot.intakeRightSlideServo.getPosition();
             //use to be 0.01
             servoposition += 0.01;
+            servoposition2 += 0.01;
+            servoposition3 += 0.01;
             robot.intakeWristServo.setPosition(Range.clip(servoposition,0,1));
+            robot.intakeLeftSlideServo.setPosition(Range.clip(servoposition2,0,1));
+            robot.intakeRightSlideServo.setPosition(Range.clip(servoposition3,0,1));
         }
 
         // gamepad2 DPAD_RIGHT for intake wrist servo
         if (gamepad_2.getButton(DPAD_RIGHT) && debounceTimer.seconds() > DEBOUNCE_THRESHOLD) {
             debounceTimer.reset();
             servoposition = robot.intakeWristServo.getPosition();
+            servoposition2 = robot.intakeLeftSlideServo.getPosition();
+            servoposition3 = robot.intakeRightSlideServo.getPosition();
             //use to be 0.01
             servoposition -= 0.01;
+            servoposition2 -= 0.01;
+            servoposition3 -= 0.01;
             robot.intakeWristServo.setPosition(Range.clip(servoposition, 0, 1));
+            robot.intakeLeftSlideServo.setPosition(Range.clip(servoposition2,0,1));
+            robot.intakeRightSlideServo.setPosition(Range.clip(servoposition3,0,1));
         }
 
         //gamepad2 X for intake claw rotation
