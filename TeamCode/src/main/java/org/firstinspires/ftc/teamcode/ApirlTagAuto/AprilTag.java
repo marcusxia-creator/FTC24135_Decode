f01a31304f2f24fc0d8b074f9a1c42cef3884014
Zirui-Ma <ziruima@outlook.com>
2025-02-09 16:42:50 -0700

    Added AprilTag test code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ApirlTagAuto/AprilTag.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.ApirlTagAuto;
+
+
+import android.util.Size;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+public class AprilTag {
+
+    private static AprilTagProcessor tagProcessor;
+    private static VisionPortal visionPortal;
+    private static HardwareMap hardwareMap;
+
+    public AprilTag(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+    }
+
+    public void init () {
+        tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Web_Cam"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+
+        visionPortal.resumeLiveView();
+        visionPortal.resumeLiveView();
+
+    }
+}
