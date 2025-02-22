7f9fa4023800f55ee7801c8b72543adc70c5dca3
gxia <xiaguowei@gmail.com>
2025-02-22 14:00:44 -0700

    update vertical slide retract position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/RobotActionConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/RobotActionConfig.java
@@ -44,7 +44,7 @@ public class RobotActionConfig {
     public static double intake_Claw_Close              = 0.27;
 
     //Deposit Config
-    public static int deposit_Slide_Down_Pos            = 15;   //unit in mm, to prevent hard hit.
+    public static int deposit_Slide_Down_Pos            = 6;   //unit in mm, to prevent hard hit.
     public static int deposit_Slide_Highbar_Pos         = 235;  //unit in mm, slides Position Configure
     public static int deposit_Slide_Highbasket_Pos      = 750; //unit in mm, highest point
     public static int deposit_Slide_Hang_Pos            = 805; //unit in mm
