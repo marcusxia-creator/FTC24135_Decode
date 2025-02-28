ecba1f764c19e448219e88a07993f07127d553cc
marcusxia-creator <129916812+marcusxia-creator@users.noreply.github.com>
2025-02-28 11:06:55 -0700

    new but not working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/drive/PinpointCumstomLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/drive/PinpointCumstomLocalizer.java
@@ -4,7 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.localization.Localizer;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class PinpointCumstomLocalizer implements Localizer {
 
@@ -32,24 +39,62 @@ public class PinpointCumstomLocalizer implements Localizer {
         pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
         //
         this.pinpoint.setOffsets(x_offset,y_offset);
-
         this.pinpoint.resetPosAndIMU();
-    }
-    @NonNull
-    @Override
-    public Pose2d getPoseEstimate() {
-        return null;
+        txWorldPinpoint = initialPose;
     }
 
     @Override
     public void setPoseEstimate(@NonNull Pose2d pose2d) {
+        double cosTheta = Math.cos(txPinpointRobot.getHeading());
+        double sinTheta = Math.sin(txPinpointRobot.getHeading());
+        double invX = - (pose2d.getX() * cosTheta + pose2d.getY() * sinTheta);
+        double invY =   pose2d.getX() * sinTheta - pose2d.getY() * cosTheta;
+        double invHeading = -pose2d.getHeading();
+        txWorldPinpoint = new Pose2d(invX, invY, invHeading);
+    }
 
+    @NonNull
+    @Override
+    public Pose2d getPoseEstimate() {
+        txWorldPinpoint = new Pose2d(pinpoint.getPosition().getX(DistanceUnit.INCH),pinpoint.getPosition().getY(DistanceUnit.INCH), pinpoint.getHeading());
+       return txWorldPinpoint;
     }
 
     @Nullable
     @Override
     public Pose2d getPoseVelocity() {
-        return null;
+        pinpoint.update();
+        if (Objects.requireNonNull(pinpoint.getDeviceStatus()) == GoBildaPinpointDriver.DeviceStatus.READY) {
+            txPinpointRobot = new Pose2d(pinpoint.getPosition().getX(DistanceUnit.INCH), pinpoint.getPosition().getY(DistanceUnit.INCH), pinpoint.getHeading());
+            Vector2d worldVelocity = new Vector2d(pinpoint.getVelocity().getX(DistanceUnit.INCH), pinpoint.getVelocity().getY(DistanceUnit.INCH));
+
+            double heading =  pinpoint.getHeading();
+            double cosA = Math.cos(heading);
+            double sinA = Math.sin(heading);
+            // Extract the world velocity components
+            double worldVx = pinpoint.getVelX()/25.4;  //convert to inch/s
+            double worldVy = pinpoint.getVelY()/25.4;  //convert to inch/s
+
+            // Apply the rotation (note the use of heading rather than -heading due to trigonometric identities)
+            double robotVx = worldVx * cosA + worldVy * sinA;
+            double robotVy = (-worldVx) * sinA + worldVy * cosA;
+
+            Vector2d robotVelocity= new Vector2d(robotVx, robotVy);
+            return new Pose2d(robotVelocity, pinpoint.getHeadingVelocity());
+        }
+        return new Pose2d(new Vector2d(0, 0), 0);
+
+    }
+
+    public static double encoderTicksToInches(double ticks) {
+        return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+    }
+    @NonNull
+    public List<Double> getWheelPositions() {
+        return Arrays.asList(
+                encoderTicksToInches(pinpoint.getEncoderX()),
+                encoderTicksToInches((pinpoint.getEncoderY())*-1)
+        );
     }
 
     @Override
