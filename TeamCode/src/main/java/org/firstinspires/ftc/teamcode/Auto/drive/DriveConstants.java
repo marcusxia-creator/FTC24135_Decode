945562bd1c56507ec9f078ade6359ffef592817a
Guowei Xia <xiaguowei@gmail.com>
2025-03-02 00:27:49 -0700

    Update Kv and Ks with Ryan Value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/drive/DriveConstants.java
@@ -56,9 +56,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.015;  // 0.012 -(20250225)
+    public static double kV = 0.012;  // 0.012 -(20250225)
     public static double kA = 0.0035;
-    public static double kStatic = 0.05;  //0.1 -(20250225); 0.05 -(20250301) - seems better on the PID
+    public static double kStatic = 0.001;  //0.001 -(20250225); 0.05 -(20250301) - seems better on the PID
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
