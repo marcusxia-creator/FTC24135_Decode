1af6380e04f1dd4506925954821af627a3758926
marcusxia-creator <129916812+marcusxia-creator@users.noreply.github.com>
2025-06-26 12:29:19 -0600

    reactive the teleop control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/AutoDriveHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/AutoDriveHandler.java
@@ -1,26 +1,20 @@
-/***package org.firstinspires.ftc.teamcode.TeleOps;
+package org.firstinspires.ftc.teamcode.TeleOps;
 
-import static org.firstinspires.ftc.teamcode.Auto.RightSideAuto_3Specimen.hSlideWaitTimer;
-import static org.firstinspires.ftc.teamcode.Auto.RightSideAuto_3Specimen.wristWaitTimer;
 import static org.firstinspires.ftc.teamcode.TeleOps.BasicTeleOps_SemiAuto.initialRun;
-
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.teamcode.Auto.PointToDrive;
-import org.firstinspires.ftc.teamcode.Auto.RightSideAuto_4Specimen_Original;
 import org.firstinspires.ftc.teamcode.Auto.drive.SampleMecanumDriveCancelable;
 import org.firstinspires.ftc.teamcode.Auto.trajectorysequence.TrajectorySequence;
 
+@Disabled
 public class AutoDriveHandler {
 
     private SampleMecanumDriveCancelable drive;
     private Pose2d poseEstimate;
     private int n;
-    private RightSideAuto_4Specimen_Original rightAuto;
-
-    private VerticalSlide vSlides;
 
     private RobotHardware robot;
 
@@ -28,18 +22,19 @@ public class AutoDriveHandler {
 
     private ElapsedTime holdTimer = new ElapsedTime();
 
+    private VerticalSlide vSlides;
+
     public AutoDriveHandler(SampleMecanumDriveCancelable drive, RobotHardware robot, int initialN, FiniteStateMachineDeposit depositArmDrive) {
         this.drive = drive;
         this.robot = robot;
         this.depositArmDrive = depositArmDrive;
-        vSlides = new VerticalSlide(this.robot);
         this.n = initialN;
         this.poseEstimate = new Pose2d(0, 0, 0); // Default placeholder pose
     }
 
     /**
      * Updates poseEstimate dynamically.
-
+    */
     public void updatePoseEstimate(Pose2d newPose) {
         this.poseEstimate = newPose;
     }
@@ -47,7 +42,7 @@ public class AutoDriveHandler {
     /**
      * Executes the auto-drive action for the Y button.
      * @return true if the auto drive was initiated.
-
+    */
     public boolean handleButtonY() {
         double X = poseEstimate.getX();
         double Y = poseEstimate.getY();
@@ -82,8 +77,7 @@ public class AutoDriveHandler {
                     robot.intakeLeftSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension_Wait);
                     robot.depositClawServo.setPosition(RobotActionConfig.deposit_Claw_Open);
                     depositArmDrive.SetDepositClawState(FiniteStateMachineDeposit.DEPOSITCLAWSTATE.OPEN);
-                    robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Left_Wait);
-                    robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Pick);
+
                 })
                 .UNSTABLE_addTemporalMarkerOffset(0, () -> {
                     vSlides.Slides_Stop();
@@ -95,18 +89,18 @@ public class AutoDriveHandler {
                 .addTemporalMarker(()->{
                     robot.intakeRightSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension);
                     robot.intakeLeftSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension);
-                    robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Left_Pick);
+
                 })
-                .waitSeconds(hSlideWaitTimer)
+                .waitSeconds(1.2)
                 .addTemporalMarker(() -> {
                     robot.intakeClawServo.setPosition(RobotActionConfig.intake_Claw_Close);
                 })
                 .waitSeconds(0.2)
                 .addTemporalMarker(() -> {
-                    robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Left_Transfer);
+                    robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Transfer);
                     robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Transfer);
                 })
-                .waitSeconds(wristWaitTimer)
+                .waitSeconds(0.25)
                 .addTemporalMarker(() -> {
                     robot.intakeRightSlideServo.setPosition(RobotActionConfig.intake_Slide_Retract);
                     robot.intakeLeftSlideServo.setPosition(RobotActionConfig.intake_Slide_Retract);
@@ -135,8 +129,8 @@ public class AutoDriveHandler {
                     robot.intakeLeftSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension_Wait);
                     robot.depositClawServo.setPosition(RobotActionConfig.deposit_Claw_Open);
                     depositArmDrive.SetDepositClawState(FiniteStateMachineDeposit.DEPOSITCLAWSTATE.OPEN);
-                    robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Left_Wait);
-                    robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Pick);
+                    robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Idle);
+                    robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Grab);
                 })
                 .UNSTABLE_addTemporalMarkerOffset(0,()->{vSlides.Slides_Stop();})
                 .waitSeconds(1)
@@ -156,18 +150,18 @@ public class AutoDriveHandler {
         robot.intakeRightSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension_Wait);
         robot.intakeLeftSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension_Wait);
         robot.depositClawServo.setPosition(RobotActionConfig.deposit_Claw_Open);
-        robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Left_Wait);
-        robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Pick);
+        robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Idle);
+        robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Grab);
     }
 
     private void intakeSpecimenPick(){
         robot.intakeRightSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension);
         robot.intakeLeftSlideServo.setPosition(RobotActionConfig.intake_Slide_Extension);
-        robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Left_Pick);
-        robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Pick);
+        robot.intakeArmServo.setPosition(RobotActionConfig.intake_Arm_Pick);
+        robot.intakeWristServo.setPosition(RobotActionConfig.intake_Wrist_Grab);
     }
 
-    /** Slides subclass for vertical slides moving helper method.
+    /** Slides subclass for vertical slides moving helper method.*/
     class VerticalSlide {
         RobotHardware robot;
         private VerticalSlide(RobotHardware robot){
@@ -197,33 +191,6 @@ public class AutoDriveHandler {
             robot.liftMotorLeft.setPower(speed);
             robot.liftMotorRight.setPower(speed);
 
-            /*while (robot.liftMotorLeft.isBusy() || robot.liftMotorRight.isBusy()) {
-                if (lSisPressed()) {  // Pass actual time
-                    Slides_Stop();
-                    break;
-                }
             }
-
-
-        }
-
     }
-    /**HELPER METHODS*/
-    /**
-     * Returns true only if the limit switch has been pressed continuously for at least 200ms.
-
-
-    private boolean lSisPressed() {
-        boolean switchState = robot.limitSwitch.getState(); // Read switch state
-
-        if (switchState) {
-            if (holdTimer.milliseconds() >= RobotActionConfig.debounceDelay) {
-                return true; // Return true only if held for at least 200ms
-            }
-        } else {
-            holdTimer.reset(); // Reset timer when switch is released
-        }
-        return false;
-    }
-} */
-
+}
