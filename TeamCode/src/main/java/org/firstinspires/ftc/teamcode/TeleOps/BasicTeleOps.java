8dd93bd27ead55895f4ec33c2e11bb968e1b4851
Guowei Xia <xiaguowei@gmail.com>
2025-10-20 23:16:29 -0600

    <Update Teleops>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/BasicTeleOps.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/BasicTeleOps.java
@@ -27,21 +27,13 @@ public class BasicTeleOps extends OpMode {
     private RobotHardware robot;
     private GamepadEx gamepadCo1, gamepadCo2;
     private RobotDrive robotDrive;
-    private FiniteStateMachineDeposit depositArmDrive;
-    private FiniteStateMachineIntake intakeArmDrive;
     private ServoTest servoTest;
-    private SlidesPIDControl slidePIDControl;
+
     private ControlState controlState = ControlState.RUN;
     private ElapsedTime debounceTimer = new ElapsedTime();
     private boolean lBstartPressed = false;
     private List<LynxModule> allHubs;
 
-    public static double kp = 3.0;
-    public static double ki = 0.0;
-    public static double kd = 0.05;
-    public static double kG =0.22;
-    public static double kV = 2;
-    public static double kS = 0.1;
 
 
     @Override
@@ -51,7 +43,6 @@ public class BasicTeleOps extends OpMode {
         robot = new RobotHardware(hardwareMap);
         robot.init(hardwareMap);
 
-        slidePIDControl = new SlidesPIDControl(robot,kp,ki,kd,RobotActionConfig.TICKS_PER_MM_SLIDES*RobotActionConfig.deposit_Slide_Highbasket_Pos,RobotActionConfig.TICKS_PER_MM_SLIDES);
 
         gamepadCo1 = new GamepadEx(gamepad1);
         gamepadCo2 = new GamepadEx(gamepad2);
@@ -59,11 +50,7 @@ public class BasicTeleOps extends OpMode {
         robotDrive = new RobotDrive(robot, gamepadCo1, gamepadCo2);
         robotDrive.Init();
 
-        depositArmDrive = new FiniteStateMachineDeposit(robot, gamepadCo1, gamepadCo2, intakeArmDrive, telemetry, slidePIDControl);
-        ///depositArmDrive.ArmInit(); did not initiate depositArm at the beginning of TeleOps
 
-        intakeArmDrive = new FiniteStateMachineIntake(robot, gamepadCo1, gamepadCo2, depositArmDrive);
-        intakeArmDrive.Init();
 
         servoTest = new ServoTest(robot, gamepadCo1, gamepadCo2);
 
@@ -79,19 +66,17 @@ public class BasicTeleOps extends OpMode {
         int lbPos = robot.backLeftMotor.getCurrentPosition();
         int rbPos = robot.backRightMotor.getCurrentPosition();
 
-        /// Lift motor encouder reading
-        int liftmotor_left_pos = robot.liftMotorLeft.getCurrentPosition();
-        int liftmotor_right_pos = robot.liftMotorRight.getCurrentPosition();
+
         telemetry.addLine("-------------------");
         telemetry.addData("Status", "initialized");
         telemetry.addData("Control Mode", robotDrive.getDriveMode().name());
-        telemetry.addData("VS Left Encoder", liftmotor_left_pos);
-        telemetry.addData("VS Right Encoder", liftmotor_right_pos);
+        telemetry.addData("Shooter Power", robot.shooterMotor.getPower());
+        telemetry.addData("Intake Motor Power", robot.intakeMotor.getPower());
         telemetry.update();
     }
     @Override
     public void start() {
-        depositArmDrive.resetHangTimer();    // ← reset your timer right when start() is called
+       ;    // ← reset your timer right when start() is called
     }
 
     @Override
@@ -119,27 +104,20 @@ public class BasicTeleOps extends OpMode {
         int lbPos = robot.backLeftMotor.getCurrentPosition();
         int rbPos = robot.backRightMotor.getCurrentPosition();
 
-        /// Lift motor encouder reading
-        int liftmotor_left_pos = robot.liftMotorLeft.getCurrentPosition();
-        int liftmotor_right_pos = robot.liftMotorRight.getCurrentPosition();
-        double vs_L_mm = liftmotor_left_pos/RobotActionConfig.TICKS_PER_MM_SLIDES;
-        double vs_R_mm = liftmotor_right_pos/RobotActionConfig.TICKS_PER_MM_SLIDES;
+
 
         /// Drive train control
         robotDrive.DriveLoop();
 
         if (gamepadCo1.getButton(BACK) && gamepadCo1.getButton(LEFT_BUMPER) && isButtonDebounced()) {
-            resetLiftEncoders();
             /// LEFT BUMPER + BACK Button for lower deposit
-            slidePIDControl.setEnabled(false);
-            depositArmDrive.ArmInit();
+
         }
 
         /// BACK Button to lower the slides
         if (gamepadCo1.getButton(BACK) && !gamepadCo1.getButton(LEFT_BUMPER) && isButtonDebounced()) {
             debounceTimer.reset();
-            slidePIDControl.setEnabled(false);
-            lowerDepositSlide();
+
         }
 
         /// START BUTTON + LEFT BUMPER to toggle control state
@@ -153,13 +131,8 @@ public class BasicTeleOps extends OpMode {
 
         /// Control condition -  Check Run Status
         if (controlState == ControlState.RUN) {
-            depositArmDrive.DepositArmLoop();
-            intakeArmDrive.IntakeArmLoop();
 
-            telemetry.addData("Deposit State", depositArmDrive.liftState);
-            telemetry.addData("Deposit Claw State", depositArmDrive.depositClawState);
-            telemetry.addData("Intake State", intakeArmDrive.intakeState);
-            telemetry.addData("Intake Claw State", intakeArmDrive.intakeClawState);
+
         }
         /// Control condition -  Check TEST Status for Servo Test
         if (controlState == ControlState.TEST) {
@@ -169,14 +142,7 @@ public class BasicTeleOps extends OpMode {
         telemetry.addLine("--------------Op Mode--------------");
         telemetry.addData("Run Mode", controlState);
         telemetry.addData("Drive Mode", robotDrive.getDriveMode().name());
-        telemetry.addData("VS Left mm", vs_L_mm);
-        telemetry.addData("VS Right mm", vs_R_mm);
-        telemetry.addData("VS Left tick", liftmotor_left_pos);
-        telemetry.addData("VS Right tick", liftmotor_right_pos);
-        telemetry.addData("targetTicks",slidePIDControl.getTargetTicks());
-        telemetry.addData("At targetTicks",slidePIDControl.atTarget());
-        telemetry.addData("slides power",slidePIDControl.getpower());
-        telemetry.addData("slides ff value",slidePIDControl.getf());
+
 
         telemetry.addData("Heading", robot.imu.getRobotYawPitchRollAngles().getYaw());
         telemetry.addLine("--------Deposit-------------");
@@ -184,13 +150,7 @@ public class BasicTeleOps extends OpMode {
         telemetry.addData("Deposit Wrist Position", robot.depositWristServo.getPosition());
         telemetry.addData("Deposit Claw Position", robot.depositClawServo.getPosition());
         telemetry.addLine("--------Intake-------------");
-        telemetry.addData("Intake Arm Left Position", robot.intakeArmServo.getPosition());
-        telemetry.addData("Intake Wrist Position", robot.intakeWristServo.getPosition());
-        telemetry.addData("Intake Claw Position", robot.intakeClawServo.getPosition());
-        telemetry.addData("Intake Slide Left Position", robot.intakeLeftSlideServo.getPosition());
-        telemetry.addData("Intake Slide Right Position", robot.intakeRightSlideServo.getPosition());
-        telemetry.addData("Intake Turret Position", robot.intakeTurretServo.getPosition());
-        telemetry.addData("Intake Rotation Position", robot.intakeRotationServo.getPosition());
+
 
         telemetry.update();
     }
@@ -201,8 +161,8 @@ public class BasicTeleOps extends OpMode {
         robot.frontRightMotor.setPower(0);
         robot.backLeftMotor.setPower(0);
         robot.backRightMotor.setPower(0);
-        robot.liftMotorLeft.setPower(0);
-        robot.liftMotorRight.setPower(0);
+        robot.shooterMotor.setPower(0);
+        robot.intakeMotor.setPower(0);
         telemetry.addData("Status", "Robot Stopped");
         telemetry.update();
     }
@@ -217,25 +177,6 @@ public class BasicTeleOps extends OpMode {
     }
      */
 
-    /// Helper -- lower deposit slide to bottom
-    private void lowerDepositSlide() {
-        robot.liftMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.liftMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.liftMotorLeft.setPower(-0.3);
-        robot.liftMotorRight.setPower(-0.3);
-    }
-
-    /// Helper -- to reset deposit slide motor encoder
-    private void resetLiftEncoders() {
-        robot.liftMotorLeft.setPower(0.0);
-        robot.liftMotorRight.setPower(0.0);
-        // 3) Now that we're at zero, actually reset the encoder count
-        robot.liftMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.liftMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // 4) Return to a normal run mode if you still want to drive by power/velocity
-        robot.liftMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.liftMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
 
     /// Helper -- to Button Debouncer
     private boolean isButtonDebounced() {
