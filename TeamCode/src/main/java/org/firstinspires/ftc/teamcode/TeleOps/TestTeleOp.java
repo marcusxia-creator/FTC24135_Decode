40a5417ffebbaa5dc2648de2a26f6efff5df2a1c
marcusxia-creator <129916812+marcusxia-creator@users.noreply.github.com>
2025-10-21 22:35:25 -0600

    Add RPM for Shooter and Intake
    Added constants for shooter and intake motor RPM conversion.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/TestTeleOp.java
@@ -22,6 +22,12 @@ public class TestTeleOp extends OpMode {
     private ElapsedTime debounceTimer = new ElapsedTime();
     private RobotDrive robotDrive;
 
+     // Example: gobilda 1150= 145.1 ticks/rev, 6000=28, GoBilda 5202/5203 = 537.7 ticks/rev.for 312rpm
+    public static double SHOOTER_TICKS_PER_REV = 28;   // change to your motor
+    public static double INTAKE_TICKS_PER_REV = 145.1;   // change to your motor
+    public static double SHOOTER_RPM_CONVERSION = 60.0 / SHOOTER_TICKS_PER_REV;
+    public static double INTAKE_RPM_CONVERSION = 60.0 / INTAKE_TICKS_PER_REV;
+
     @Override
     public void init() {
         gamepad_1 = new GamepadEx(gamepad1);
@@ -37,10 +43,24 @@ public class TestTeleOp extends OpMode {
         robot.leftGateServo.setPosition(RobotActionConfig.gateDown);
         robot.rightGateServo.setPosition(RobotActionConfig.gateDown);
         robot.spindexerServo.setPosition(RobotActionConfig.spindexerSlot1);
+
+        robot.shooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Determine if Intake is jammed
+        boolean isJamed = false;
     }
 
     @Override
     public void loop() {
+        // === Read velocity in ticks/sec and convert to RPM ===
+        double shooter_ticksPerSec = robot.shooterMotor.getVelocity();
+        double shooter_rpm = shooter_ticksPerSec * SHOOTER_RPM_CONVERSION;
+
+        double intake_ticksPerSec = robot.intakeMotor.getVelocity();
+        double intake_rpm = intake_ticksPerSec * INTAKE_RPM_CONVERSION;
+       
         if (gamepad_1.getButton(GamepadKeys.Button.A) && isButtonDebounced()) {
             //servoposition = robot.pushRampServo.getPosition() + 0.01;
             //robot.pushRampServo.setPosition(Range.clip(servoposition, 0.0, 1.0));
@@ -78,24 +98,31 @@ public class TestTeleOp extends OpMode {
             robot.shooterMotor.setPower(0);
         }
         if (gamepad_2.getButton(GamepadKeys.Button.DPAD_LEFT) && isButtonDebounced()){
-            robot.intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
             speed = robot.intakeMotor.getPower() + 0.05;
-            robot.intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             robot.intakeMotor.setPower(Range.clip(speed,0.5,1.0));
         }
         if (gamepad_2.getButton(GamepadKeys.Button.DPAD_RIGHT) && isButtonDebounced()){
             robot.intakeMotor.setPower(0);
         }
+
+        // Telemetry (DS + Dashboard)       
         telemetry.addData("Ramp Position", robot.pushRampServo.getPosition());
         telemetry.addData("Left Gate Position", robot.leftGateServo.getPosition());
         telemetry.addData("Right Gate Position", robot.rightGateServo.getPosition());
         telemetry.addData("Spindexer Position", robot.spindexerServo.getPosition());
+        telemetry.addline("----Shooter----");
         telemetry.addData("Shooter Speed", robot.shooterMotor.getPower());
+        telemetry.addData("Shooter Ticks/sec", "%.1f", shooter_ticksPerSec);
+        telemetry.addData("Shooter RPM", "%.1f", shooter_rpm);
+        telemetry.addline("----Shooter----");
         telemetry.addData("Intake Speed", robot.intakeMotor.getPower());
+        telemetry.addData("Intake Ticks/sec", "%.1f", intake_ticksPerSec);
+        telemetry.addData("Intake RPM", "%.1f", intake_rpm);
 
         telemetry.update();
     }
 
+    // Button Debounce Helper
     private boolean isButtonDebounced() {
         if (debounceTimer.seconds() > RobotActionConfig.DEBOUNCE_THRESHOLD) {
             debounceTimer.reset();
@@ -103,6 +130,15 @@ public class TestTeleOp extends OpMode {
         }
         return false;
     }
+
+    // Intake Jam Helper
+    private boolean isJammed() {
+        if (intake_rpm < RobotActionConfig.DEBOUNCE_THRESHOLD) {
+            return true;
+        }
+        return false;
+    }
+    
 }
 
 
