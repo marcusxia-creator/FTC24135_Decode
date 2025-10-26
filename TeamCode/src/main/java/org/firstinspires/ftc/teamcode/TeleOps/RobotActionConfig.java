41f702de8ca4b518c2a99d92a7046527d023a115
ThatOneIdiot333 <ken.ma.yyc@gmail.com>
2025-10-26 14:18:06 -0600

    tester teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/RobotActionConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/RobotActionConfig.java
@@ -7,7 +7,9 @@ public class RobotActionConfig {
     public static double gateDown                       = 0.0;
     public static double gateUp                         = 1.0;
     public static double DEBOUNCE_THRESHOLD             = 0.25;
-    public static double spindexerIncrement             = 0.333;
+    public static double spindexerSlot1                 = 0.0;
+    public static double spindexerSlot2                 = 0.46;
+    public static double spindexerSlot3                 = 0.92;
     public static double spindexerReset                 = 0.0;
     public static double rampResetPos                   = 0.45; //change to 0.45
     public static double rampUpPos                      = 0.15;
