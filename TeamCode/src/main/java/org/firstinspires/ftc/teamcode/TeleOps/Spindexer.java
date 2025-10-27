0c1b1b4f540e36ccd09cf7686c8aa6764df17506
Sylvan-Z <Sylvanz20@gmail.com>
2025-10-26 21:52:37 -0600

    Spindexer class, need colour sensor testing.
    Very simple changes in teleop using this class fsms will optimize process

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/Spindexer.java
@@ -1,11 +1,81 @@
 package org.firstinspires.ftc.teamcode.TeleOps;
 
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
 public class Spindexer {
+    public enum SLOT{
+        Empty,
+        Green,
+        Purple
+    }
 
+    public RobotHardware robot;
+    public SLOT[] slots;
+    public int currentSlot;
+    //For jams
+    public int prevSlot;
 
+    Spindexer(SLOT slot1,SLOT slot2,SLOT slot3, int currentSlot){
+        slots = new SLOT[]{slot1, slot2, slot3};
+        this.currentSlot = currentSlot;
+    }
+
+    public void writeToCurrent(SLOT a){
+        slots[currentSlot]=a;
+    }
+
+    public void writeToCurrent(ColourCriterion[] criteria){
+        //not yet implimented
+    }
+
+    public int count(SLOT a){
+        int counter = 0;
+        for(SLOT slot:slots){
+            if(slot==a){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public Boolean checkFor(SLOT a){
+        return count(a)>0;
+    }
+
+    public void runToSlot(int n){
+        prevSlot = currentSlot;
+        currentSlot = (n-1)%3+1;
+        if(n==1){
+            robot.spindexerServo.setPosition(RobotActionConfig.spindexerSlot1);
+        }
+        if(n==2){
+            robot.spindexerServo.setPosition(RobotActionConfig.spindexerSlot2);
+        }
+        if(n==3){
+            robot.spindexerServo.setPosition(RobotActionConfig.spindexerSlot3);
+        }
+    }
+
+    public Boolean runToSlot(SLOT a){
+        if(checkFor(a)){
+            int n;
+            int distance = 4;
+
+            //look for closest slot
+            for(int i=0; i<=2; i++){
+                if(slots[i]==a && Math.abs(i-currentSlot)<=distance){
+                    distance=Math.abs(i-currentSlot);
+                    n=i;
+                }
+            }
+
+            runToSlot(n);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void unJam(){
+        runToSlot(prevSlot);
+    }
 }
