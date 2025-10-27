955c6cfe22402cffafffec152009b71d187368bc
marcusxia-creator <129916812+marcusxia-creator@users.noreply.github.com>
2025-10-27 16:16:19 -0600

    <Add - Shared Ball List>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/SharedBallList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/SharedBallList.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.TeleOps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SharedBallList {
+    private final List<Ball> balls = new ArrayList<>();
+
+    public SharedBallList(double[] slotAngles) {
+        for (int i = 0; i < slotAngles.length; i++) {
+            balls.add(new Ball("Empty", i, slotAngles[i], false));
+        }
+    }
+
+    public List<Ball> getBalls() { return balls; }
+
+    public void resetAll() {
+        for (Ball b : balls) {
+            b.setHasBall(false);
+            b.setBallColor("Empty");
+        }
+    }
 }
