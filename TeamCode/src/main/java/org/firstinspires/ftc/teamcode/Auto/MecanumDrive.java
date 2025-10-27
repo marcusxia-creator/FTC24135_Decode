b8ff9b9c6aa92edcf185c08c9c68154f33feef40
ryanxia <ryanzihengxia@gmail.com>
2025-10-26 21:11:42 -0600

    Tuned Feedforwards and PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/MecanumDrive.java
@@ -68,9 +68,9 @@ public final class MecanumDrive {
         public double trackWidthTicks = 6645.1554072606;
 
         // feedforward parameters (in tick units)
-        public double kS = 1.2019010748077927;
-        public double kV = 0.0002464207294423375;
-        public double kA = 0;
+        public double kS = 1.7;
+        public double kV = 0.000225;
+        public double kA = 0.000005;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,13 +82,13 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 0;
+        public double lateralGain = 0;
+        public double headingGain = 4.5; // shared with turn
 
-        public double axialVelGain = 0.0;
-        public double lateralVelGain = 0.0;
-        public double headingVelGain = 0.0; // shared with turn
+        public double axialVelGain = 0;
+        public double lateralVelGain = 0;
+        public double headingVelGain = 0.25;
     }
 
     public static Params PARAMS = new Params();
