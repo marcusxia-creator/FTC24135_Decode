a2a3961a2355ea61975de8b8e032a3eb4fd7ee6b
gxia <xiaguowei@gmail.com>
2025-10-27 23:58:58 -0600

    <revamp SharedBallList with new ball class>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/SharedBallList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/SharedBallList.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SharedBallList {
+
     private final List<Ball> balls = new ArrayList<>();
 
     public SharedBallList(double[] slotAngles) {
         for (int i = 0; i < slotAngles.length; i++) {
-            balls.add(new Ball("Empty", i, slotAngles[i], false));
+            balls.add(new Ball(i, slotAngles[i]));
         }
     }
 
-    public List<Ball> getBalls() { return balls; }
-
-    public void resetAll() {
-        for (Ball b : balls) {
-            b.setHasBall(false);
-            b.setBallColor("Empty");
-        }
+    public List<Ball> getBalls() {
+        return balls;
     }
 }
