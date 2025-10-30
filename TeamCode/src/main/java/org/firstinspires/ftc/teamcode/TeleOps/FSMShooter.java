e8d1f6b0fec4d2050d5f2a50624fcfc599707622
gxia <xiaguowei@gmail.com>
2025-10-30 10:22:03 -0600

    <debug - optimize offtake control flow and add comments>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/FSMShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/FSMShooter.java
@@ -2,14 +2,14 @@ package org.firstinspires.ftc.teamcode.TeleOps;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import static org.firstinspires.ftc.teamcode.TeleOps.RobotActionConfig.*;
+
+
 
 public class FSMShooter {
     private final GamepadEx gamepad_1;
-    private final GamepadEx gamepad_2;
     private final RobotHardware robot;
     private ElapsedTime debounceTimer = new ElapsedTime();
     private ElapsedTime shootTimer = new ElapsedTime();
@@ -18,91 +18,130 @@ public class FSMShooter {
     public RAMPSTATE rampstate = RAMPSTATE.DOWN;
     private int counter = 2;
 
+    /**
+     * BUTTON FOR SHOOTING
+     * * Button A is global KEY, --- Global Controls (can be triggered from any state) ---
+     *   cancel the actionS & Exit the loop for this cycle
+     * * Button X/Square is local key, --- IDLE STATE---
+     *   Press 'X/Square' to start spinning the flywheel
+     * * Button X/Square is local key, --- FLYWHEEL STATE---
+     *   Cancle the FLYWHEEL within 1 second
+     * * Button Y/Triangle is local key, --- RAMPUP STATE---
+     *   Press 'Y/Triangle' to toggle ramp up to launch ball to shooter
+     */
 
-   // double [] isFilled = {false, false, false};
-    double [] slotAngle = {0.02, 0.46, 0.90};
 
     public enum SHOOTERSTATE {
+        IDLE,
         FLYWHEEL_START,
         RAMP_UP,
         DISTANCE_SENSOR_CHECK,
         SPINDEXER_ROTATE,
+        COOLDOWN
     }
     public enum RAMPSTATE{
         UP,
         DOWN
     }
 
+    // Define slotAngle
+    double [] slotAngle;
+
     public FSMShooter(GamepadEx gamepad_1, GamepadEx gamepad_2, RobotHardware robot) {
         this.gamepad_1 = gamepad_1;
-        this.gamepad_2 = gamepad_2;
         this.robot = robot;
+        slotAngle = new double[]{spindexerSlot1, spindexerSlot2, spindexerSlot3};
     }
 
     public void Init() {
         counter = 2;
         robot.shooterMotor.setPower(0);
-        robot.pushRampServo.setPosition(RobotActionConfig.rampDownPos);
-        shooterState = SHOOTERSTATE.FLYWHEEL_START;
-        robot.leftGateServo.setPosition(RobotActionConfig.gateDown);
-        robot.rightGateServo.setPosition(RobotActionConfig.gateDown);
-        robot.spindexerServo.setPosition(RobotActionConfig.spindexerSlot1);
+        robot.pushRampServo.setPosition(rampDownPos);
+        robot.leftGateServo.setPosition(gateDown);
+        robot.rightGateServo.setPosition(gateDown);
+        robot.spindexerServo.setPosition(slotAngle[counter]);
+        shooterState = SHOOTERSTATE.IDLE;;
     }
 
     public void ShooterLoop() {
+        // --- Global Controls (can be triggered from any state) ---
+        // 'A' button is an emergency stop or reset.
+        if (gamepad_1.getButton(GamepadKeys.Button.A) && isButtonDebounced()) {
+            Init();
+            return; // Exit the loop for this cycle
+        }
         switch (shooterState) {
-            case FLYWHEEL_START:
+            case IDLE:
+                // Press 'X' to start spinning the flywheel
                 if (gamepad_1.getButton(GamepadKeys.Button.X) && isButtonDebounced()) {
-                    robot.shooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                    robot.shooterMotor.setPower(RobotActionConfig.shooterSpeed);
-                    if (gamepad_1.getButton(GamepadKeys.Button.Y) && isButtonDebounced()) {
-                        shooterState = SHOOTERSTATE.RAMP_UP;
-                    }
+                    robot.shooterMotor.setPower(shooterSpeed);
                     shootTimer.reset();
+                    shooterState = SHOOTERSTATE.FLYWHEEL_START;
+                }
+                break;
+            case FLYWHEEL_START:
+                // Give the flywheel time ProcessBuilder.Redirect.to get ProcessBuilder.Redirect.to speed (Log.e.g., 1 second)
+                if (shootTimer.seconds() > 1.0) {
+                    shooterState = SHOOTERSTATE.RAMP_UP;
+                }
+                // Press 'X' again to stop spinning the flywheel
+                // Allow driver to turn off flywheel if they change their mind with in 1 second
+                if (gamepad_1.getButton(GamepadKeys.Button.X) && isButtonDebounced()) {
+                    shooterState = SHOOTERSTATE.COOLDOWN;
                 }
                 break;
             case RAMP_UP:
-                toggleRamp();
-                updateServoState();
-                shootTimer.reset();
+                // Press 'Y' to toggle ramp up/down
+                if (gamepad_1.getButton(GamepadKeys.Button.Y) && isButtonDebounced()) {
+                    rampstate = RAMPSTATE.UP;
+                    updateServoState();
+                    shootTimer.reset();
+                    shooterState = SHOOTERSTATE.DISTANCE_SENSOR_CHECK;
+                }
                 break;
             case DISTANCE_SENSOR_CHECK:
-                if (shootTimer.seconds() > 0.5) {
-                    toggleRamp();
-                    updateServoState();
+                if (shootTimer.seconds() > 0.5 ) {
+                        rampstate = RAMPSTATE.DOWN;
+                        updateServoState();
+                }
+                if(shootTimer.seconds() > 1.0){
                     double distance = robot.distanceSensor.getDistance(DistanceUnit.MM);
                     if (distance < 100) {
                         shooterState = SHOOTERSTATE.RAMP_UP;
                         shootTimer.reset();
                     } else {
-                        robot.pushRampServo.setPosition(RobotActionConfig.rampDownPos);
                         shooterState = SHOOTERSTATE.SPINDEXER_ROTATE;
                         shootTimer.reset();
                     }
                 }
                 break;
             case SPINDEXER_ROTATE:
-                if (counter > 0) {
-                    counter = counter - 1;
+                // Decrement counter to aim at the next slot
+                counter--;
+                if (counter >= 0) {
                     robot.spindexerServo.setPosition(slotAngle[counter]);
-                    if (shootTimer.seconds() > 0.2) {
+                    if (shootTimer.seconds() > 0.45) {
                         shooterState = SHOOTERSTATE.RAMP_UP;
                     }
-                } else if (counter == 0) {
-                    shooterState = SHOOTERSTATE.FLYWHEEL_START;
+                } else {
+                    shooterState = SHOOTERSTATE.COOLDOWN;
                 }
                 shootTimer.reset();
                 break;
+            case COOLDOWN:
+                // Spin down the motor and reset state to IDLE
+                robot.shooterMotor.setPower(0);
+                // After cooling down, we go back to the beginning.
+                // Re-initialize to reset the counter for the next cycle.
+                Init();
+                shooterState = SHOOTERSTATE.IDLE;
+                break;
             default:
-                shooterState = SHOOTERSTATE.FLYWHEEL_START;
-        }
-        if (gamepad_1.getButton(GamepadKeys.Button.A) && isButtonDebounced()){
-            robot.pushRampServo.setPosition(RobotActionConfig.rampDownPos);
-            robot.shooterMotor.setPower(0);
-            shooterState = SHOOTERSTATE.FLYWHEEL_START;
+                shooterState = SHOOTERSTATE.IDLE;
         }
     }
-    private void toggleRamp (){
+
+    private void toggleRamp(){
         if (rampstate == RAMPSTATE.UP){
             rampstate = RAMPSTATE.DOWN;
         }else{
@@ -116,7 +155,6 @@ public class FSMShooter {
             robot.pushRampServo.setPosition(RobotActionConfig.rampUpPos);
         }
     }
-
 
     private boolean isButtonDebounced() {
         if (debounceTimer.seconds() > RobotActionConfig.DEBOUNCE_THRESHOLD) {
