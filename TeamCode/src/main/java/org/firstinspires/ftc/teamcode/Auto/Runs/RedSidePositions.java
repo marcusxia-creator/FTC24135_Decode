d635cbdd39f922b7d74332788983dcaabc4118b3
ryanxia <ryanzihengxia@gmail.com>
2025-11-07 20:09:01 -0700

    Added all Roadrunner files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Runs/RedSidePositions.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Auto.Runs;
+
+public class RedSidePositions {
+    public static double CloseShotPower = 0.78;
+    public static double FarShotPower = 0.95;
+
+    public static double IntakeSet1Position1_X = 34;
+    public static double IntakeSet1Position1_Y = 32;
+    public static double IntakeSet1Position2_X = 34;
+    public static double IntakeSet1Position2_Y = 37;
+    public static double IntakeSet1Position3_X = 34;
+    public static double IntakeSet1Position3_Y = 42;
+    public static double IntakeSet1Position4_X = 34;
+    public static double IntakeSet1Position4_Y = 60;
+
+    public static double IntakeSet2Position1_X = 10;
+    public static double IntakeSet2Position1_Y = 32;
+    public static double IntakeSet2Position2_X = 10;
+    public static double IntakeSet2Position2_Y = 37;
+    public static double IntakeSet2Position3_X = 10;
+    public static double IntakeSet2Position3_Y = 42;
+    public static double IntakeSet2Position4_X = 10;
+    public static double IntakeSet2Position4_Y = 60;
+
+    public static double TransitionLocation_X = 12;
+    public static double TransitionLocation_Y = 12;
+
+
+    public static double ShootingPosition_X = -12;
+    public static double ShootingPosition_Y = 12;
+    public static double ShootingPosition_Heading = -45;
+
+
+
+}
