41f418f62b330132dad90458ca65bc0594664005
ThatOneIdiot333 <ken.ma.yyc@gmail.com>
2025-12-27 18:27:07 -0700

    <Update> - Set up new classes for interleague - 12/27/2025

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/Turret.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.TeleOps;
+
+public class Turret {
+    /*We will control the turret using input from the pinpoint
+    turret is continuously running and is separate from the shooter control
+
+     */
+}
