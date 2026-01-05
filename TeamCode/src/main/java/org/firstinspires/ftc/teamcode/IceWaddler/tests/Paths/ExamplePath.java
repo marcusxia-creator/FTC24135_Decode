4c538048b5f300c0e42368e6ea06cc6fec8f6b14
Sylvan-Z <Sylvanz20@gmail.com>
2026-01-04 17:06:40 -0700

    More debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IceWaddler/tests/Paths/ExamplePath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IceWaddler/tests/Paths/ExamplePath.java
@@ -21,17 +21,17 @@ public class ExamplePath {
     public static double Ball1X = 1;
     public static double Ball2X = 1.1;
     public static double Ball3X = 1.2;
-    public static double intakeTime = 0.2; //time delay until spindexer
-    public static double shootTime = 1; //time delay until spindexer
+    public static double intakeTime = 0.2; //time delay until spindexer, replace with action
+    public static double shootTime = 1; //time delay for shoot, replace with action
     public static double row1Y = 0.7;
     public static Pose2D shootingPose = new Pose2D(METER, 0, 1, AngleUnit.DEGREES,-45);
-    public static double fastSpeed = 0.8;
+    public static double fastSpeed = 0.6;
     public static double slowSpeed = 0.3;
 
     //Example shooting run
     public static List<Action> ShootPath = new ArrayList<>(Arrays.asList(
             new Action(init,                                                                new Pose2D(METER, decelX, row1Y, AngleUnit.DEGREES, 180),   fastSpeed,  false,  0.1, new String[]{}),
-            new Action(new Pose2D(METER, decelX, row1Y, AngleUnit.DEGREES, -170),    new Pose2D(METER, Ball1X, row1Y, AngleUnit.DEGREES, 180),   slowSpeed,  true,   stopTolerance,new String[]{}),
+            new Action(new Pose2D(METER, decelX, row1Y, AngleUnit.DEGREES, -160),    new Pose2D(METER, Ball1X, row1Y, AngleUnit.DEGREES, 180),   slowSpeed,  true,   stopTolerance,new String[]{}),
             new Action(HOLDTYPE.POS, intakeTime, new String[]{"Intake"}),
             new Action(new Pose2D(METER, Ball1X, row1Y, AngleUnit.DEGREES, 180),    new Pose2D(METER, Ball2X, row1Y, AngleUnit.DEGREES, 180),   slowSpeed,  true,   stopTolerance,new String[]{}),
             new Action(HOLDTYPE.POS, intakeTime, new String[]{"Intake"}),
