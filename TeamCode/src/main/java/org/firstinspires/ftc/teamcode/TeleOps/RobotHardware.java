cfb00a3c74448ae4011aa8609cf9432e62988ac0
gxia <xiaguowei@gmail.com>
2026-01-25 17:45:03 -0700

    <Update> retract time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/RobotHardware.java
@@ -218,7 +218,7 @@ public class RobotHardware {
     public void initPinpoint() {
         pinpoint.setOffsets(38.1, -184.15, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
         pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
-        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
         pinpoint.resetPosAndIMU();
     }
 
