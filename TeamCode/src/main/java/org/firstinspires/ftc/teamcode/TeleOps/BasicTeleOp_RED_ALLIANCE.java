401b472220d1e06b3323bc4a693fe5aa70ca7b4c
gxia <xiaguowei@gmail.com>
2026-07-20 00:48:49 -0600

    remove not used import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/BasicTeleOp_RED_ALLIANCE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/BasicTeleOp_RED_ALLIANCE.java
@@ -19,11 +19,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Auto.Runs.commonclasses.PoseStorage;
-import org.firstinspires.ftc.teamcode.TeleOps.Sensors.BallColor;
-import org.firstinspires.ftc.teamcode.TeleOps.Sensors.ColorDetection;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Making the sequence shooting go slot 5-4-3 (intaking) 3-2-1 (shooting)
